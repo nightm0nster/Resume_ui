d222e118d2227b62fc2347ece6a34c38c13f435a
pey2 <108261096+pey2@users.noreply.github.com>
2023-02-16 03:11:47 +0800

    w/ textbox

--- ResumeFinal/Resume/window.py
+++ ResumeFinal/Resume/window.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 
 def btn_clicked():
@@ -19,7 +20,30 @@ canvas = Canvas(
     relief = "ridge")
 canvas.place(x = 0, y = 0)
 
-img0 = PhotoImage(file = f"img0.png")
+style = ttk.Style()
+style.theme_use("default")
+style.configure("Treeview",
+                background="#E7F6F2",
+                rowheight=25,
+                fieldbackground="#E7F6F2")
+my_tree = ttk.Treeview()
+my_tree['columns'] = ("File Name", "Qualification")
+
+my_tree.column("#0", width=30, minwidth=25)
+my_tree.column("File Name", width=100, anchor=W)
+my_tree.column("Qualification", anchor=CENTER, width=100)
+
+# Headings
+my_tree.heading("#0", text="#", anchor=CENTER)
+my_tree.heading("File Name", text="File Name", anchor=CENTER)
+my_tree.heading("Qualification", text="Qualification", anchor=CENTER)
+
+my_tree.insert(parent='', index='end', iid=0, text="1", values=("Resume", "50%"))
+my_tree.place(x=447, y=70, width=319, heigh=281)
+
+# my_tree.pack()
+
+img0 = PhotoImage(file = "img0.png")
 btnOpen = Button(
     image = img0,
     borderwidth = 0,
@@ -32,7 +56,18 @@ btnOpen.place(
     width = 190,
     height = 43)
 
-imgSearch = PhotoImage(file = f"imgSearch.png")
+lblFile = Label(
+    font=("Archivo Black",15, "bold"),
+    bg="#2c3333",
+    fg="white",
+    text="File Path:"
+)
+
+lblFile.place(
+    x=240,
+    y=30
+)
+imgSearch = PhotoImage(file = "imgSearch.png")
 btnSearch = Button(
     image = imgSearch,
     borderwidth = 0,
@@ -45,7 +80,7 @@ btnSearch.place(
     width = 190,
     height = 43)
 
-imgClear = PhotoImage(file = f"imgClear.png")
+imgClear = PhotoImage(file = "imgClear.png")
 btnClear = Button(
     image = imgClear,
     borderwidth = 0,
@@ -57,6 +92,8 @@ btnClear.place(
     x = 512, y = 367,
     width = 190,
     height = 43)
+
+
 
 window.resizable(False, False)
 window.mainloop()
